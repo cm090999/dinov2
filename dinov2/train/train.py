4076a8ad20d392bec646e77548efda879c89b099
colin <cmerk@ethz.ch>
2025-05-23 13:25:26 +0200

    log knn to csv

--- dinov2/train/train.py
+++ dinov2/train/train.py
@@ -54,23 +54,31 @@ def periodic_eval(model, cfg, iteration, step, aim_run=None):
     
     # Run eval
     results_dict_knn = eval_knn(eval_model,eval_train_dataset,eval_val_dataset,accuracy_averaging=AccuracyAveraging.MEAN_ACCURACY,nb_knn=(10, 20, 100, 200),temperature=0.07,batch_size=256,num_workers=8,gather_on_cpu=False,)
-    metrics_file_path = os.path.join(cfg.train.output_dir, "eval", f"eval_results_iteration_{iteration}_step_{step}.json")
+    metrics_file_path = os.path.join(cfg.train.output_dir, "eval", f"eval_results_iteration_{iteration}_step_{step}.csv")
     
-    results_dict = {}
+    import pandas as pd
+    knn_df = []
     if distributed.is_main_process():
         for neighbor in results_dict_knn.keys():
             for topx in results_dict_knn[neighbor].keys():
                 neighbor_name = neighbor[1]
                 log_name = f"knn_neighbors_{neighbor_name}_top_{topx}"
                 result_value = results_dict_knn[neighbor][topx].item()
+                #append row with neighbor, topx, and result_value
+                knn_df.append({"neighbor": neighbor_name, "topx": topx, "result_value": result_value})
                 if aim_run:
                     aim_run.track(result_value, name=f'eval/{log_name}', step=step, context={"subset": "eval"})
 
-    with open(metrics_file_path, "a") as f:
-        for k, v in results_dict.items():
-            f.write(json.dumps({k: v}) + "\n")
-            if aim_run and distributed.is_main_process(): # Also log to aim here
-                aim_run.track(v, name=f'eval/{k}', step=step, context={"subset": "eval"})
+    if distributed.is_main_process():
+        # Save results to CSV
+        knn_df = pd.DataFrame(knn_df)
+        knn_df.to_csv(metrics_file_path, index=False)
+        logger.info(f"Saved KNN results to {metrics_file_path}")
+    # with open(metrics_file_path, "a") as f:
+    #     for k, v in results_dict_knn.items():
+    #         f.write(json.dumps({k: v}) + "\n")
+    #         if aim_run and distributed.is_main_process(): # Also log to aim here
+    #             aim_run.track(v, name=f'eval/{k}', step=step, context={"subset": "eval"})
 
     if distributed.is_enabled():
         torch.distributed.barrier()
